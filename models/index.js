ebef0b4cf31ba1e8f2db29babb516258d266ee95
mcabrex <mrcabardo@gmail.com>
2017-10-03 18:05:35 -0400

    push

---
+++ models/index.js
@@ -0,0 +1,89 @@
+const Sequelize = require('sequelize')
+const db = new Sequelize('postgres://localhost:5432/name',{
+  logging: false
+})
+
+
+let Place = db.define({
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  city: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  phone: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  location: {
+    type: Sequelize.ARRAY(Sequelize.FLOAT),
+    allowNull: false
+  }
+})
+
+let Hotel = db.define({
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  num_stars: {
+    type: Sequelize.FLOAT,
+    validate: {
+      min: 1,
+      max: 5
+    },
+    allowNull: false
+  },
+  amenities: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+})
+
+let Activity = db.define({
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age_range: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+})
+
+let Restaurant = db.define({
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  cuisine: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 1,
+      max: 5
+    },
+    allowNull: false
+  }
+})
+
+Hotel.hasMany(Place);
+Restaurant.hasMany(Place);
+Activity.hasMany(Place);
+
+module.exports = {
+  db: db,
+  Place: Place,
+  Hotel: Hotel,
+  Activity: Activity,
+  Restaurant: Restaurant
+}
